4b31559f40a12f3de31b2afaca3f0710024322a8
silvercondor <biocrypto@protonmail.com>
2021-06-11 23:46:41 +0800

    Add full path

--- V2/Flash Loan - Batch/FlashLoanReceiverBase.sol
+++ V2/Flash Loan - Batch/FlashLoanReceiverBase.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.6.12;
 
 import {IFlashLoanReceiver, ILendingPoolAddressesProvider, ILendingPool, IERC20  } from "https://github.com/silvercondor/code-examples-protocol/blob/main/V2/Flash%20Loan%20-%20Batch/Interfaces.sol";
-import { SafeERC20, SafeMath } from "Libraries.sol";
+import { SafeERC20, SafeMath } from "https://github.com/silvercondor/code-examples-protocol/blob/main/V2/Flash%20Loan%20-%20Batch/Libraries.sol";
 
 abstract contract FlashLoanReceiverBase is IFlashLoanReceiver {
   using SafeERC20 for IERC20;
