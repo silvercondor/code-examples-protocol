2b64f95ac868201f1bc2bbf438eee2762610cb62
silvercondor <biocrypto@protonmail.com>
2021-06-11 23:49:41 +0800

    Bump version and remove irrelevant override

--- V2/Flash Loan - Batch/FlashLoanReceiverBase.sol
+++ V2/Flash Loan - Batch/FlashLoanReceiverBase.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: agpl-3.0
-pragma solidity 0.6.12;
+pragma solidity >=0.6.12;
 
 import {IFlashLoanReceiver, ILendingPoolAddressesProvider, ILendingPool, IERC20  } from "https://github.com/silvercondor/code-examples-protocol/blob/main/V2/Flash%20Loan%20-%20Batch/Interfaces.sol";
 import { SafeERC20, SafeMath } from "https://github.com/silvercondor/code-examples-protocol/blob/main/V2/Flash%20Loan%20-%20Batch/Libraries.sol";
@@ -8,8 +8,8 @@ abstract contract FlashLoanReceiverBase is IFlashLoanReceiver {
   using SafeERC20 for IERC20;
   using SafeMath for uint256;
 
-  ILendingPoolAddressesProvider public immutable override ADDRESSES_PROVIDER;
-  ILendingPool public immutable override LENDING_POOL;
+  ILendingPoolAddressesProvider public immutable ADDRESSES_PROVIDER;
+  ILendingPool public immutable LENDING_POOL;
 
   constructor(ILendingPoolAddressesProvider provider) public {
     ADDRESSES_PROVIDER = provider;
